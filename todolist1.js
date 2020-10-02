efad40b471b1fd4b9acbac44a1b388bbd262a3b4
Travis Bowlby <tarv@traviss-mbp.myfiosgateway.com>
2020-10-01 20:32:15 -0400

    updated todo list and refactored

--- todolist1.js
+++ todolist1.js
@@ -5,16 +5,38 @@ var input = prompt("What would you like to do?");
 while (input !== "quit") {
     // handle input
     if(input === "list") {
-        console.log(todos);
+        listTodos();
     } else if(input === "new") {
-        // ask for new todo
-        var newTodo = prompt("Enter new todo");
-        // add new todo to array
-        todos.push(newTodo);
+        addTodo();
+    } else if(input === "delete") {
+        deleteTodo();
     }
-
     // ask again for new input
     input = prompt("What would you like to do?");
     
 }
 console.log("OK YOU QUIT THE APP");
+
+function listTodos() {
+    console.log("**********");
+    todos.forEach(function(todo, i) {
+    console.log("**********");
+    console.log(i + ": " + todo);
+    })
+}
+
+function addTodo() {
+    // ask for new todo
+    var newTodo = prompt("Enter new todo");
+    // add new todo to array
+    todos.push(newTodo);
+    console.log("Added todo");
+}
+
+function deleteTodo() {
+    // ask for index of todo to be deleted
+    let index = prompt("Enter index of Todo to delete");
+    // delete todo
+    todos.splice(index,1);
+    console.log("Deleted todo");
+}
